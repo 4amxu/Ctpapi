b0d42deb13c333e2308633394eaefee755ac9747
4amxu <68855118+4amxu@users.noreply.github.com>
2021-02-21 12:24:57 +0800

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,11 @@
+
+Config = {'brokerid':'9999', 'userid':'127922', 'password':'668868', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10130', 'md_address':'tcp://180.168.146.187:10131'}
+# Config = {'brokerid':'9999', 'userid':'127922', 'password':'668868', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10101', 'md_address':'tcp://180.168.146.187:10111'}
+
+'''
+# Config 配置模板
+Config = {'brokerid':'9999', 'userid':'123456', 'password':'******', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10130', 'md_address':'tcp://180.168.146.187:10131'}
+Config = {'brokerid':'9999', 'userid':'127922', 'password':'******', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10101', 'md_address':'tcp://180.168.146.187:10111'}
+'''        
+    
+    
