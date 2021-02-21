4985c8c3a3ce19329de30db6a2a0585d0d7b9fa6
4amxu <68855118+4amxu@users.noreply.github.com>
2021-02-21 12:26:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class MACDStrategy(Strategy):
 if __name__ == '__main__':
     # Config 配置模板
     Config = {'brokerid':'9999', 'userid':'123456', 'password':'******', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10130', 'md_address':'tcp://180.168.146.187:10131'}
-    # Config = {'brokerid':'9999', 'userid':'127922', 'password':'******', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10101', 'md_address':'tcp://180.168.146.187:10111'} 
+    # Config = {'brokerid':'9999', 'userid':'123456', 'password':'******', 'appid':'simnow_client_test', 'auth_code':'0000000000000000', 'product_info':'python dll', 'td_address':'tcp://180.168.146.187:10101', 'md_address':'tcp://180.168.146.187:10111'} 
     t = CTP(MACDStrategy())
     t.Login(Config)   
     
