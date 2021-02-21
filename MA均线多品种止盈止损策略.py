a525880d7db4e0993f65b97481f3772da931cc1d
4amxu <68855118+4amxu@users.noreply.github.com>
2021-02-21 12:47:14 +0800

    Update MA均线多品种止盈止损策略.py

--- MA均线多品种止盈止损策略.py
+++ MA均线多品种止盈止损策略.py
@@ -8,7 +8,7 @@ class MACDStrategy(Strategy):
     def __init__(self):
         super().__init__()
         self.symbol_lsit = ["m2105","ni2104","rb2105","ag2106","IF2103","IC2103","i2105","j2105"]  #订阅合约
-        self.bar_time = BarType.Time15  #订阅K线周期  秒级 BarType.Time3  Time5  Time15  Time30       分钟级  BarType.Min、  Min3 、 Min5 、 Min15 、 Min30 、 Min60
+        self.BarType = BarType.Time15  #订阅K线周期  秒级 BarType.Time3  Time5  Time15  Time30       分钟级  BarType.Min、  Min3 、 Min5 、 Min15 、 Min30 、 Min60
         self.volume = {
         "m2105":{"总持仓":3,"止损":0,"止盈":0,"移动止损":0},
         "ni2104":{"总持仓":1,"止损":0,"止盈":0,"移动止损":0},
